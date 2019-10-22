679f9674f9d11cc5fa4ef3039d79b5a88bb15243
patrick.dai <17081581@suning.com>
2019-10-22 17:49:35 +0800

    The first user Android Studio create project.

---
+++ app/src/main/java/dai/android/ndk/opengle/es/MainActivity.java
@@ -0,0 +1,14 @@
+package dai.android.ndk.opengle.es;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
